ebb980a673af30b62bbd6c29c8caaae9895eb08b
Brayan Zuniga <brayanmzg9503@gmail.com>
2023-06-28 19:48:24 -0500

    Actualizacion calculadora prueba

--- src/main/java/co/com/ps/proyectoFinal/Operaciones.java
+++ src/main/java/co/com/ps/proyectoFinal/Operaciones.java
@@ -2,8 +2,8 @@ package co.com.ps.proyectoFinal;
 
 public class Operaciones {
 
-    double num1;
-    double num2;
+    //double num1;
+    //double num2;
 
     double suma;
     double resta;
@@ -37,13 +37,13 @@ public class Operaciones {
         return potencia;
     }
 
-    public double getNum1() {
-        return num1;
-    }
+    //public double getNum1() {
+    //    return num1;
+    //}
 
-    public double getNum2() {
-        return num2;
-    }
+    //public double getNum2() {
+    //    return num2;
+    //}
 
     public double getSuma() {
         return suma;
@@ -65,12 +65,12 @@ public class Operaciones {
         return potencia;
     }
 
-    public void setNum1(double num1) {
-        this.num1 = num1;
-    }
+    //public void setNum1(double num1) {
+     //   this.num1 = num1;
+    //}
 
-    public void setNum2(double num2) {
-        this.num2 = num2;
-    }
+    //public void setNum2(double num2) {
+      //  this.num2 = num2;
+    //}
 
 }
